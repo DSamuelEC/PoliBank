376f1372495cbc27223831b0cf8fbe6aa65d159c
MariOvallos <167836177+MariOvallos@users.noreply.github.com>
2024-09-27 18:14:45 -0500

    nuevas clases

---
+++ src/co/edu/unbosque/model/persistence/PersonaDAO.java
@@ -0,0 +1,43 @@
+package co.edu.unbosque.model.persistence;
+
+import co.edu.unbosque.model.Persona;
+
+public class PersonaDAO implements InterfaceDAO<Persona>{
+
+	
+	
+	public PersonaDAO() {
+		
+	}
+	
+	@Override
+	public String getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean add(Persona x) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean delete(Persona x) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean update(Persona x, Persona y) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Persona find(Persona x) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
