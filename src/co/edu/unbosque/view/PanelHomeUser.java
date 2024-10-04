e4b0a9f8b3353b297495cae7b99ba4d3cc886bb2
MariOvallos <167836177+MariOvallos@users.noreply.github.com>
2024-10-03 19:31:32 -0500

    PanelHomeUser

--- src/co/edu/unbosque/view/PanelHomeUser.java
+++ src/co/edu/unbosque/view/PanelHomeUser.java
@@ -4,69 +4,84 @@ import javax.swing.*;
 import java.awt.*;
 
 public class PanelHomeUser extends JPanel {
-    private JButton btnAgregarPareja, btnActualizarPareja, btnEliminarPareja, btnSalir, btnVerTodo;
-    private JTextField txtAliasPareja, txtCupoPareja;
-    private JTable tableParejas;
+	private JButton btnAgregarPareja, btnActualizarPareja, btnEliminarPareja, btnSalir, btnVerTodo;
+	private JTextField txtAliasPareja, txtCupoPareja;
+	private JTable tableParejas;
 
-    public PanelHomeUser() {
-        setLayout(null);
-        setBackground(new Color(41, 119, 255));
-        inicializarComponentes();
-        setVisible(false);
-    }
+	public PanelHomeUser() {
+		setLayout(null);
+		setBackground(new Color(41, 119, 255));
+		inicializarComponentes();
+		setVisible(false);
+	}
 
-    private void inicializarComponentes() {
-        // Panel para agregar nueva pareja
-        JPanel panelFormularioParejas = new JPanel(null);
-        panelFormularioParejas.setBounds(10, 10, 470, 100);
-        panelFormularioParejas.setBackground(new Color(200, 200, 255));  // Un color diferente para visualizar el panel
+	private void inicializarComponentes() {
 
-        JLabel lblAlias = new JLabel("Alias de la Pareja:");
-        lblAlias.setBounds(10, 10, 120, 25);
-        txtAliasPareja = new JTextField(15);
-        txtAliasPareja.setBounds(140, 10, 150, 25);
+		JPanel Usuario = new JPanel(null);
+		Usuario.setBounds(8, 10, 470, 24);
+		Usuario.setBackground(new Color(200, 200, 255));
 
-        JLabel lblCupo = new JLabel("Cupo en Dólares:");
-        lblCupo.setBounds(10, 40, 120, 25);
-        txtCupoPareja = new JTextField(5);
-        txtCupoPareja.setBounds(140, 40, 150, 25);
+		JLabel lblUserName = new JLabel("Usuario: ");
+		lblUserName.setBounds(5, 1, 120, 25);
 
-        btnAgregarPareja = new JButton("Agregar Pareja");
-        btnAgregarPareja.setBounds(300, 10, 160, 25);
+		JLabel lblCupoUsuario = new JLabel("Cupo: ");
+		lblCupoUsuario.setBounds(270, 1, 120, 25);
 
-        panelFormularioParejas.add(lblAlias);
-        panelFormularioParejas.add(txtAliasPareja);
-        panelFormularioParejas.add(lblCupo);
-        panelFormularioParejas.add(txtCupoPareja);
-        panelFormularioParejas.add(btnAgregarPareja);
+		Usuario.add(lblCupoUsuario);
+		Usuario.add(lblUserName);
 
-        // Tabla para mostrar las parejas
-        tableParejas = new JTable();
-        JScrollPane scrollPane = new JScrollPane(tableParejas);
-        scrollPane.setBounds(11, 120, 467, 150);
+		// Panel para agregar nueva pareja
+		JPanel panelFormularioParejas = new JPanel(null);
+		panelFormularioParejas.setBounds(8, 42, 470, 100);
+		panelFormularioParejas.setBackground(new Color(200, 200, 255)); // Un color diferente para visualizar el panel
 
-        // Panel para botones
-        JPanel panelBotones = new JPanel(null);
-        panelBotones.setBounds(10, 280, 470, 85);
-        panelBotones.setBackground(new Color(200, 200, 255)); 
-        btnActualizarPareja = new JButton("Actualizar Pareja");
-        btnActualizarPareja.setBounds(5, 10, 150, 25);
-        btnEliminarPareja = new JButton("Eliminar Pareja");
-        btnEliminarPareja.setBounds(165, 10, 150, 25);
-        btnVerTodo = new JButton("Ver Todo");
-        btnVerTodo.setBounds(325, 10, 140, 25);
-        btnSalir = new JButton("Salir");
-        btnSalir.setBounds(165, 50, 150, 25); 
+		JLabel lblAlias = new JLabel("Alias de la Pareja:");
+		lblAlias.setBounds(10, 10, 120, 25);
+		txtAliasPareja = new JTextField(15);
+		txtAliasPareja.setBounds(140, 10, 150, 25);
 
-        panelBotones.add(btnActualizarPareja);
-        panelBotones.add(btnEliminarPareja);
-        panelBotones.add(btnSalir);
-        panelBotones.add(btnVerTodo); 
+		JLabel lblCupo = new JLabel("Cupo en Dólares:");
+		lblCupo.setBounds(10, 40, 120, 25);
+		txtCupoPareja = new JTextField(5);
+		txtCupoPareja.setBounds(140, 40, 150, 25);
 
-        add(panelFormularioParejas);
-        add(scrollPane);
-        add(panelBotones);
-    }
+		btnAgregarPareja = new JButton("Agregar Pareja");
+		btnAgregarPareja.setBounds(300, 10, 160, 25);
+
+		panelFormularioParejas.add(lblAlias);
+		panelFormularioParejas.add(txtAliasPareja);
+		panelFormularioParejas.add(lblCupo);
+		panelFormularioParejas.add(txtCupoPareja);
+		panelFormularioParejas.add(btnAgregarPareja);
+
+		// Tabla para mostrar las parejas
+		tableParejas = new JTable();
+		JScrollPane scrollPane = new JScrollPane(tableParejas);
+		scrollPane.setBounds(8, 150, 470, 180);
+
+		// Panel para botones
+		JPanel panelBotones = new JPanel(null);
+		panelBotones.setBounds(8, 340, 470, 85);
+		panelBotones.setBackground(new Color(200, 200, 255));
+		btnActualizarPareja = new JButton("Actualizar Pareja");
+		btnActualizarPareja.setBounds(5, 10, 150, 25);
+		btnEliminarPareja = new JButton("Eliminar Pareja");
+		btnEliminarPareja.setBounds(165, 10, 150, 25);
+		btnVerTodo = new JButton("Ver Todo");
+		btnVerTodo.setBounds(325, 10, 140, 25);
+		btnSalir = new JButton("Salir");
+		btnSalir.setBounds(165, 50, 150, 25);
+
+		panelBotones.add(btnActualizarPareja);
+		panelBotones.add(btnEliminarPareja);
+		panelBotones.add(btnSalir);
+		panelBotones.add(btnVerTodo);
+
+		add(panelFormularioParejas);
+		add(scrollPane);
+		add(panelBotones);
+		add(Usuario);
+	}
 
 	public JButton getBtnAgregarPareja() {
 		return btnAgregarPareja;
@@ -131,5 +146,5 @@ public class PanelHomeUser extends JPanel {
 	public void setTableParejas(JTable tableParejas) {
 		this.tableParejas = tableParejas;
 	}
-    
+
 }
